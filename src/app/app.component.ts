d3f88a37109ff4eab62f9ee7844db8f61d478231
capb90 <capb90@hotmail.com>
2021-05-13 15:39:12 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import {QrScannerComponent} from 'angular2-qrscanner';
+import { Router } from '@angular/router';
+import {ConfirmationService} from 'primeng/api';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+ 
 })
-export class AppComponent {
-  title = 'QrCodeRead';
+export class AppComponent implements OnInit {
+    
+    @ViewChild(QrScannerComponent,{static:false}) qrScannerComponent!: QrScannerComponent ;
+  
+  
+    constructor() {}
+  
+    ngOnInit() {
+      
+  }
+  
+    ngAfterViewInit(): void {
+      this.qrScannerComponent.getMediaDevices().then(devices => {
+          const videoDevices: MediaDeviceInfo[] = [];
+          for (const device of devices) {
+              if (device.kind.toString() === 'videoinput') {
+                  videoDevices.push(device);
+              }
+          }
+          if (videoDevices.length > 0){
+              let choosenDev;
+              for (const dev of videoDevices){
+                  if (dev.label.includes('front')){
+                      choosenDev = dev;
+                      break;
+                  }
+              }
+              if (choosenDev) {
+                  this.qrScannerComponent.chooseCamera.next(choosenDev);
+              } else {
+                  this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+              }
+          }
+      });
+  
+      this.qrScannerComponent.capturedQr.subscribe(dataQR => {
+       
+       console.log(dataQR)
+      });
+     
+       
+    }
+  
+  
 }
